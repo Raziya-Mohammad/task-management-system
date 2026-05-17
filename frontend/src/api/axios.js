86cb844244f5ded30c1f4ebb82107bf03a280664
Raziya <raziya.md2006@gmail.com>
2026-05-17 17:56:19 +0530

    Connect frontend to Render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:"https://task-management-system-mu5b.onrender.com/api"
 });
 
 API.interceptors.request.use((req)=>{
 
-const token=
-localStorage.getItem("token");
+const token=localStorage.getItem("token");
 
 if(token){
 
-req.headers.Authorization=
-`Bearer ${token}`;
+req.headers.Authorization=`Bearer ${token}`;
 
 }
 
